9ef3c557cce80861a694ad5ff17f1d08550f8c42
cesonu <154247256+cesonu@users.noreply.github.com>
2025-03-28 16:44:02 +0000

    First commit

---
+++ backend/server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const jwt = require('jsonwebtoken');
+
+app.use(express.json()); // For parsing application/json
+
+// MongoDB Atlas connection string
+const dbURI = 'mongodb+srv://comfortesonu57:Said0804*@contactmanagementapplic.8nsicau.mongodb.net/';
+
+// Connect to MongoDB Atlas
+mongoose.connect(dbURI)
+  .then(() => console.log('MongoDB Atlas connected'))
+  .catch((err) => console.log('Error connecting to MongoDB Atlas:', err));
+
+// Define the Contact Schema
+const contactSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  phone: String,
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+// Routes
+
+// Get All Contacts
+app.get('/contacts', async (req, res) => {
+    console.log('GET /contacts endpoint hit');
+    try {
+    const contacts = await Contact.find();
+    res.json(contacts);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// Create Contact
+app.post('/contacts', async (req, res) => {
+    console.log('POST /contacts endpoint hit');
+    try {
+    const { name, email, phone } = req.body;
+    const contact = new Contact({ name, email, phone });
+    await contact.save();
+    res.status(201).json(contact);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
